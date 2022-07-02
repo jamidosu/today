2c7cbe13966655b7354eb64c1166b6183ae236cc
saju4488 <107746711+saju4488@users.noreply.github.com>
2022-07-03 03:33:54 +0900

    jami 2 commit

--- today.py
+++ today.py
@@ -10,23 +10,23 @@ window = Tk()
 window.title("astrology")
 
 
-Canvas = Canvas(window, height=680, width=680, bg="ivory")
+Canvas = Canvas(window, height=950, width=950, bg="ivory")
 Canvas.pack()
 
-img_main = PhotoImage(file="jini.png")
-canvas_img = Canvas.create_image(340, 340, image=img_main)
-Canvas.create_text(340, 30, text="소원을 말해봐", fill="brown",
+img_main = PhotoImage(file="asas.png")
+canvas_img = Canvas.create_image(475, 475, image=img_main)
+Canvas.create_text(475, 30, text="소원을 말해봐", fill="brown",
                    font=("나눔바른펜", 30, "bold"))
 
 img_list = []
-for i in range(3):
+for i in range(10):
     img = PhotoImage(file=f"images/img{i}.png")
     img_list.append(img)
 
 button = Button(window, text="Click", bg="white", fg="hot pink",
                 font=("나눔바른펜", 20, "bold"),
                 command=rand_choice)
-button.place(x=610, y=0)
+button.place(x=885, y=0)
 
 
 window.mainloop()
